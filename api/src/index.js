750a3b2f302439ea3b9c3f6edc9408979803a0bd
ArtemkaXD <shadextx@gmail.com>
2022-03-08 19:26:17 +0300

    plug MongoDB and update to CRUD

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,48 @@
+require('dotenv').config();
 const express = require('express');
+const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
+const { ApolloServer } = require('apollo-server-express');
+
+let notes = [
+  {
+    id: '1',
+    content: 'This is a note',
+    author: 'Adam Scott'
+  },
+  {
+    id: '2',
+    content: 'This is another note',
+    author: 'Harlow Everly'
+  },
+  {
+    id: '3',
+    content: 'Oh hey look, another note!',
+    author: 'Riley Harrison'
+  }
+];
+
+db.connect(DB_HOST);
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Fuck the world!'));
-app.listen(4000, () => console.log('Listen on port 4000!'));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return { models };
+  }
+});
+
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen(4000, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
