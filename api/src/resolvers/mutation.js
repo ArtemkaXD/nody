83803ee67f43af6f6855bc8fada9f528bd05eb04
ArtemkaXD <shadextx@gmail.com>
2022-04-13 19:00:58 +0300

    mutation

--- api/src/resolvers/mutation.js
+++ api/src/resolvers/mutation.js
@@ -1,3 +1,13 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+  AuthenticationError,
+  ForbiddenError
+} = require('apollo-server-express');
+require('dotenv').config();
+
+const gravatar = require('../util/gravatar')
+
 module.exports = {
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
